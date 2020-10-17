f8f096847e8e23ca00f8a02a50971d4129140f5e
Serhii_Tomchuk <dice1@ukr.net>
2020-10-17 20:26:10 +0300

    Merge master branch

--- blog_test/urls.py
+++ blog_test/urls.py
@@ -4,6 +4,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
 
+    path('blog/', include('blog.urls')),
     path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html')),
